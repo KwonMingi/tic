535f2c253f9aaa6dc6fd0ee4d267d6d199ab315a
KwonMingi <aabkji89@gmail.com>
2023-06-27 13:03:41 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:tctk/constants/gaps.dart';
+import 'package:tctk/constants/sizes.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: Row(children: const [
+            Text('hello'),
+            Gaps.h20,
+            Text('hello'),
+          ]),
+        ),
+      ),
+    );
+  }
+}
